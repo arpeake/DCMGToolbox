cd5e2c782ba5eacb6d49aabc68cd8e6b4c94df32
Alan Peake <arpeake83@gmail.com>
2016-09-01 01:40:03 -0400

    Add project files.

---
+++ DCMGToolbox/ActiveDirectory.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices.ActiveDirectory;
+using System.DirectoryServices;
+
+namespace DCMGToolbox
+{
+    public class ActiveDirectory
+    {
+        public string GetDomainName()
+        {
+            string domainName = String.Empty;
+
+            IPGlobalProperties ipgp = IPGlobalProperties.GetIPGlobalProperties();
+            domainName = ipgp.DomainName;
+
+            return domainName;
+        }
+
+        public void CreateUser(string username, string firstName, string lastName, string password, string emailAddress = "", bool passNeverExpires = false, bool userCannotChangePW = false, bool changePWOnNextLogon = false)
+        {
+            using (var pc = new PrincipalContext(ContextType.Domain))
+            {                
+                using (var up = new UserPrincipal(pc))
+                {                    
+                    up.SamAccountName = username;
+                    up.EmailAddress = emailAddress;
+                    up.SetPassword(password);
+                    up.Enabled = true;
+                    up.PasswordNeverExpires = passNeverExpires;
+                    up.UserCannotChangePassword = userCannotChangePW;
+                    if (changePWOnNextLogon)
+                        up.ExpirePasswordNow();
+                    up.Save();
+                }
+            }
+        }
+
+        public string GetDomainDN(string domain)
+        {
+            string DN = String.Empty;
+
+            DirectoryContext context = new DirectoryContext(DirectoryContextType.Domain, domain);
+            Domain d = Domain.GetDomain(context);
+            DirectoryEntry de = d.GetDirectoryEntry();
+            return de.Properties["DistinguishedName"].Value.ToString();
+        }
+
+        public List<string> GetOUList(string domain)
+        {
+            List<string> OUs = new List<string>();
+            string dn = GetDomainDN(domain);
+            DirectoryEntry startingPoint = new DirectoryEntry("LDAP://DC=YourCompany,DC=com");
+
+            DirectorySearcher searcher = new DirectorySearcher(startingPoint);
+            searcher.Filter = "(objectCategory=organizationalUnit)";
+
+            foreach (SearchResult res in searcher.FindAll())
+            {
+                OUs.Add(res.Path);
+            }
+
+            return OUs;
+        }
+    }
+}
---
+++ DCMGToolbox/Network.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+
+namespace DCMGToolbox
+{
+    public class Network
+    {
+        public bool Pingable(string iporhostname)
+        {
+            bool pingable = false;
+            Ping pinger = new Ping();
+
+            try
+            {
+                PingReply reply = pinger.Send(iporhostname);
+                pingable = reply.Status == IPStatus.Success;
+            }
+            catch(NullReferenceException)
+            {
+                pingable = false;
+            }
+
+            return pingable;
+        }
+    }
+}
